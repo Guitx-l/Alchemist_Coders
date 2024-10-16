c18728a9d439d81de85b47bff719c8a1f3d4c1b8
Guitx-l <guilhem.bontemps@orange.fr>
2024-10-16 17:33:32 +0200

    Update python.py
    copie de toutes les fonction du pilote rsk

--- python.py
+++ python.py
@@ -1,4 +1,7 @@
 import rsk
+from rsk import constants
+
+
 
 with rsk.Client() as client:
     client.green1.kick()
@@ -27,5 +30,60 @@ client.green1.pose
 # Ball's position (x [m], y [m])
 client.ball
 
+# Field length (x axis)
+constants.field_length
+# Field width (y axis)
+constants.field_width
+
+# Goal width
+constants.goal_width
+
+# Side of the (green) border we should be able to see around the field
+constants.border_size
 
 
+
+# Kicks, takes an optional power parameter between 0 and 1
+robot.kick()
+
+# Controls the robots in its own frame, arguments are x speed [m/s],
+# y speed [m/s] and rotation speed [rad/s]
+
+# Go forward, 0.25 m/s
+robot.control(0.25, 0., 0.)
+
+# Rotates 30 deg/s counter-clockwise
+robot.control(0., 0., math.radians(30))
+
+
+
+
+try:
+    robot.kick()
+    robot.control(0.1, 0, 0)
+except rsk.ClientError:
+    print('Error during a command!')
+
+
+
+
+# Sending a robot to x=0.2m, y=0.5m, theta=1.2 rad
+robot.goto((0.2, 0.5, 1.2))
+
+
+
+# Placing two robots, the loop will block until both robots are placed
+arrived = False
+while not arrived:
+    robot_1_arrived = client.green1.goto((0.2, 0.3, 0.), wait=False)
+    robot_2_arrived = client.green2.goto((0.2, -0.3, 0.), wait=False)
+    arrived = robot_1_arrived and robot_2_arrived
+
+
+
+
+def print_the_ball(client, dt):
+    print(client.ball)
+
+# This will print the ball everytime a new information is obtained from the client
+client.on_update = print_the_ball

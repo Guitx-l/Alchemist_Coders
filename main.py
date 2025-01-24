a1a468bf14ef388ab4869b124be611d21c8b16f7
Guitx-l <guilhem.bontemps@orange.fr>
2025-01-24 13:51:41 +0300

    Added main file

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from math import pi
 import time
 import main_shooter
 
-threading.Thread(main_shooter.main())
+threading.Thread(target=lambda *_: (print("main shooter start"), main_shooter.main("-t green -r".split(" ")))).start()
+
+import rsk
+from rsk import constants
+from math import pi
+import time
 
 color = "blue"
 team = "teams"
@@ -23,28 +28,30 @@ i = 0
 
 def defenseur():
 
-    cages(pBalle)
-    dB2 = [pbut,yCage,0]
+    try:
+        cages(pBalle)
+        dB2 = [pbut,yCage,0]
 
 
-        
-    if(not refRobot2["penalized"] and not refRobot2["preempted"]) :
-        print("if rbt2 fonctionne")
-        robot2.goto((dB2), wait=False)
+            
+        if(not refRobot2["penalized"] and not refRobot2["preempted"]) :
+            #print("if rbt2 fonctionne")
+            robot2.goto((dB2), wait=False)
+            arrived = False
+        else :
+            print(refRobot1["penalized"], " and ", refRobot1["preempted"])
         arrived = False
-    else :
-        print(refRobot1["penalized"], " and ", refRobot1["preempted"])
-    
 
 
-    while not arrived :
-        robot_2_arrived = robot2.goto((dB2), wait=False)
-        arrived = robot_2_arrived
-        if arrived :
-            print(f"pp1 = {round(pp1[0],2), round(pp1[1],2), round(pp1[2],2)} ;") 
-            print(f"pBalle {round(pBalle[0],2), round(pBalle[1],2)} ;\n")
-            robot1.kick()
-            arrived = True
+        while not arrived :
+            robot_2_arrived = robot2.goto((dB2), wait=False)
+            arrived = robot_2_arrived
+            if arrived :
+                #print(f"pp1 = {round(pp1[0],2), round(pp1[1],2), round(pp1[2],2)} ;") 
+                #print(f"pBalle {round(pBalle[0],2), round(pBalle[1],2)} ;\n")
+                arrived = True
+    except rsk.client.ClientError as e:
+        print(f"eror: {e}")
 
 
 
@@ -66,3 +73,62 @@ def cages(pBalle):
     
 
     return(yCage)
+
+
+with rsk.Client(host='127.0.0.1', key='') as client:
+    try:
+        robot1 = client.robots[color][1]
+        robot2 = client.robots[color][2]
+        robotennemis1 = client.robots[colorennemis][1]
+        robotennemis2 = client.robots[colorennemis][2]
+        ref = client.referee
+        refRobot1 = client.referee[team][color]["robots"]['1']
+        refRobot2 = client.referee[team][color]["robots"]['2']
+        """Il suffira de multiplier les x des robot et le l'emplacement des cages
+        par x_pos pour obenir la position souhaité en fonction du coté de notre camp"""
+        x_positive = ref[team][color]["x_positive"]
+        if (x_positive == True) :
+            x_pos = -1
+        else :
+            x_pos = 1
+
+        robot1.pose
+    
+    except Exception as e :
+        print("erreur début with rsk.client... : ",e)
+
+    
+    arrived = True
+
+    print("arrivée")
+    if(not ref["game_is_running"]):
+        while ref["game_is_running"] == False :
+            print(i)
+            i += 1
+            time.sleep(0.01)
+            print("game isn't running")
+            if(ref["game_is_running"]):
+                i += 1000
+                break
+
+    else :
+        while True :
+
+            try:
+                # Position + orientation (x [m], y [m], theta [rad])
+                # position p -> piloté  e -> ennemis
+                pp1 = robot1.pose
+                pp2 = robot2.pose
+                pe1 = robotennemis1.pose
+                pe2 = robotennemis2.pose
+                pBalle = client.ball
+                # postition que le défenceur doit prendre pour se positionner au niveaux des cages
+                # ATTENTION pbut = - constants.defense_area_width car constants.defense_area_width 
+                pbut = - constants.defense_area_width * x_pos
+
+            except Exception as e :
+                print("erreur début while true : ",e)
+
+            cages(pBalle)
+
+            defenseur()

64601b4a2c445e59d8de71926fb301db0d73fa80
Jonathan-Mamba <jojodiallo.jd@gmail.com>
2025-05-29 12:55:30 +0200

    Changed is_shooter() behavior

--- src/multi_client.py
+++ src/multi_client.py
@@ -5,6 +5,20 @@ from shooter import BaseShooterClient, MainShooterClient, RotatedShooterClient
 from goalkeeper import BaseGoalKeeperClient, MainGoalKeeperClient, RotatedGoalKeeperClient
 from typing import Literal, Callable
 
+
+
+def is_shooter(client: rsk.Client, team: str, number: int, ball: util.array, goal_sign: int) -> bool:
+    bot = client.robots[team][number]
+    other_bot = client.robots[team][3 - number]
+
+    if -0.46 < ball[0] * goal_sign < 0:
+        return bot.position[0] * goal_sign > other_bot.position[0] * goal_sign
+    elif ball[0] * goal_sign > 0:
+        return True
+    return False
+
+
+
 class BaseMultiClient(util.BaseClient, abc.ABC):
     def __init__(self,
                  client: rsk.Client,
@@ -12,25 +26,17 @@ class BaseMultiClient(util.BaseClient, abc.ABC):
                  number: Literal[1, 2],
                  shooter_client: BaseShooterClient,
                  keeper_client: BaseGoalKeeperClient,
-                 is_shooter: Callable[[util.array, int], bool] | None = None
                  ) -> None:
         super().__init__(client, team)
         self.number = number
         self.shooter_client = shooter_client
         self.keeper_client = keeper_client
 
-        if is_shooter:
-            self.is_shooter = is_shooter
-        elif number == 1:
-            self.is_shooter = lambda ball, goal_sign: ball[0] * goal_sign > -0.46
-        else:
-            self.is_shooter = lambda ball, goal_sign: ball[0] * goal_sign > 0
-
         self.shooter_client.shooter = self.client.robots[team][number]
         self.keeper_client.keeper = self.client.robots[team][number]
 
     def get_client(self) -> util.BaseClient:
-        if self.is_shooter(self.ball, self.goal_sign()):
+        if is_shooter(self.client, self.shooter_client.shooter.team, self.number, self.ball, self.goal_sign()):
             return self.shooter_client
         return self.keeper_client
 

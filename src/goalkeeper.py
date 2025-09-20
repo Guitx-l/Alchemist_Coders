f79273f1a8c656dddd3d4ebd173cde3696807e3d
Jonathan-Mamba <135890743+Jonathan-Mamba@users.noreply.github.com>
2025-09-20 16:20:42 +0200

    Clogging fix 2

--- src/goalkeeper.py
+++ src/goalkeeper.py
@@ -1,4 +1,3 @@
-import abc
 import enum
 import time
 import numpy as np
@@ -10,10 +9,10 @@ from typing import Literal
 
 class Strategy(enum.Enum):
     NONE = enum.auto()
-    BALL = enum.auto()
+    BALL_RUSH = enum.auto()
     TAN_SHOOTER = enum.auto()
     THALES_SHOOTER = enum.auto()
-    THALES_BALL = enum.auto()
+    BALL_VECTOR = enum.auto()
     BALL_BEHIND = enum.auto()
     PROJECT = enum.auto()
 
@@ -45,35 +44,36 @@ class GoalKeeperClient(util.BaseClient):
             self.keeper.goto(self.keeper.pose)
             return
 
-        shooter = self.get_opposing_shooter().pose
+        opp_shooter = self.get_opposing_shooter().pose
+        team_mate = self.client.robots[self.team][3 - self.keeper.number].position
         goal_post_x = -0.92 * self.goal_sign()
-        no_shooter = np.linalg.norm(self.ball - shooter[:2]) > 0.18
+        no_shooter = np.linalg.norm(self.ball - opp_shooter[:2]) > 0.18
 
         if no_shooter and (np.linalg.norm(self.ball - self.last_ball_position) > 0.05):
             ball_vector = self.ball - self.last_ball_position
             target_y = self.ball[1] + (ball_vector[1] * (goal_post_x - self.last_ball_position[0]) / ball_vector[0])
-            self.strategy = Strategy.THALES_BALL
+            self.strategy = Strategy.BALL_VECTOR
             
-        elif no_shooter and (self.ball[0] * self.goal_sign() < 0.2):
+        elif no_shooter and (self.ball[0] * self.goal_sign() < 0.2) and (np.linalg.norm(self.ball - team_mate) < 0.15):
             target_x, target_y = self.ball
-            self.strategy = Strategy.BALL
+            self.strategy = Strategy.BALL_RUSH
 
         elif faces_ball(self.get_opposing_shooter(), self.ball, 20):
-            target_y = shooter[1] + (math.tan(shooter[2]) * (goal_post_x - shooter[0]))
+            target_y = opp_shooter[1] + (math.tan(opp_shooter[2]) * (goal_post_x - opp_shooter[0]))
             self.strategy = Strategy.TAN_SHOOTER
 
-        elif util.get_alignment(np.array([target_x, target_y]), self.ball, shooter[:2]) < 10:
-            target_y = self.ball[1] + (self.ball[1] - shooter[1]) / (self.ball[0] - shooter[0]) * (goal_post_x - self.ball[0])
+        elif util.get_alignment(np.array([target_x, target_y]), self.ball, opp_shooter[:2]) < 10:
+            target_y = self.ball[1] + (self.ball[1] - opp_shooter[1]) / (self.ball[0] - opp_shooter[0]) * (goal_post_x - self.ball[0])
             self.strategy = Strategy.THALES_SHOOTER
 
 
-        if not self.is_inside_defense_zone(self.ball) and self.ball[0] * self.goal_sign() < 0.1 and self.strategy not in (Strategy.NONE, Strategy.BALL):
+        if not self.is_inside_defense_zone(self.ball) and self.ball[0] * self.goal_sign() < 0.1 and self.strategy not in (Strategy.NONE, Strategy.BALL_RUSH):
             goal_pos = np.array([target_x, target_y])
             trajectory = self.ball - goal_pos
             new_target = goal_pos + trajectory * (np.dot(self.keeper.position - goal_pos, trajectory) / np.linalg.norm(trajectory) ** 2)
             target_x, target_y = new_target
             self.strategy = Strategy.PROJECT
-        elif self.strategy != Strategy.BALL:
+        elif self.strategy != Strategy.BALL_RUSH:
             target_y = np.clip(target_y, -0.25, 0.25)
 
         ball_vector = self.keeper.position - self.ball
--- src/multi_client.py
+++ src/multi_client.py
@@ -9,21 +9,17 @@ from typing import Literal
 def can_play(bot: rsk.client.ClientRobot, referee: dict) -> bool:
     return (not referee['teams'][bot.team]['robots'][str(bot.number)]['preempted']) and (not referee['teams'][bot.team]['robots'][str(bot.number)]['penalized'])
 
-def is_shooter(client: rsk.Client, team: str, number: int, ball: util.array, goal_sign: int) -> bool:
+def is_shooter(client: rsk.Client, team: str, number: int, goal_sign: int) -> bool:
     bot = client.robots[team][number]
     other_bot = client.robots[team][3 - number]
 
     if not can_play(other_bot, client.referee):
-        return ball[0] * goal_sign > 0
+        return client.ball[0] * goal_sign > 0
     return bot.position[0] * goal_sign > other_bot.position[0] * goal_sign
 
 
 class MultiClient(util.BaseClient):
-    def __init__(self,
-                 client: rsk.Client,
-                 team: Literal['green', 'blue'],
-                 number: Literal[1, 2],
-                 ) -> None:
+    def __init__(self, client: rsk.Client, team: Literal['green', 'blue'], number: Literal[1, 2]) -> None:
         super().__init__(client, team)
         self.number = number
         self.shooter_client = ShooterClient(client, team)
@@ -33,7 +29,7 @@ class MultiClient(util.BaseClient):
         self.keeper_client.keeper = self.client.robots[team][number]
 
     def get_client(self) -> util.BaseClient:
-        if is_shooter(self.client, self.shooter_client.shooter.team, self.number, self.ball, self.goal_sign()):
+        if is_shooter(self.client, self.team, self.number, self.goal_sign()):
             return self.shooter_client
         return self.keeper_client
 
